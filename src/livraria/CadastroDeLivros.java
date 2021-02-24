92ac7b12b0394a79b595fee5406ef1766ca22116
Rodrigo-Abreuu <77403600+Rodrigo-Abreuu@users.noreply.github.com>
2021-02-24 15:15:45 -0300

    Criando a classe Autor

--- src/livraria/CadastroDeLivros.java
+++ src/livraria/CadastroDeLivros.java
@@ -4,12 +4,18 @@ public class CadastroDeLivros {
 
 	public static void main(String[] args) {
 		
+	Autor autor = new Autor();
+	autor.nome = "Rodrigo Abreu";
+	autor.email = "rodrigão@gmiau.com";
+	autor.cpf = "040.028.922";
+	
 	Livro livro = new Livro();
 	livro.nome = "Java 8 Prático";
 	livro.descricao  = "Novos recursos da linguagem";
 	livro.valor = 59.90;
 	livro.isbn = "978-85-66250-46-6";
-
+	livro.autor = autor;
+	
 	livro.mostrarDetalhes();
 	
 	System.out.println();
@@ -19,6 +25,7 @@ public class CadastroDeLivros {
 	outroLivro.descricao = "Crie seus primeiros programas";
 	outroLivro.valor = 59.90;
 	outroLivro.isbn = "978-85-66250-22-0";
+	outroLivro.autor = autor;
 	
 	outroLivro.mostrarDetalhes();
 	}
--- src/livraria/Livro.java
+++ src/livraria/Livro.java
@@ -6,14 +6,29 @@ public class Livro {
 	String descricao;
 	double valor;
 	String isbn;
+	Autor autor;
+	
+	boolean temAutor(){
+		return this.autor != null;
+	}
 	
 	void mostrarDetalhes(){
 		String mensagem = "Mostrando detalhes do livro ";
 		System.out.println(mensagem);
-		System.out.println(nome);
-		System.out.println(descricao);
-		System.out.println(valor);
-		System.out.println(isbn);
+		System.out.println("Nome: " + nome);
+		System.out.println("Descrição: " + descricao);
+		System.out.println("Valor: " + valor);
+		System.out.println("ISBN: " + isbn);
+		
+		if (this.temAutor()) {
+			autor.mostrarDetalhes();
+		}
+		
+		System.out.println("---");
+	}
+	
+	public void aplicarDescontoDe(double porcentagem){
+		this.valor -= this.valor * porcentagem;
 	}
 	
 }
