a42d2c21a135ff83cb9168227a4a79128e4d2052
Rodrigo-Abreuu <77403600+Rodrigo-Abreuu@users.noreply.github.com>
2021-02-24 16:59:16 -0300

    Criação de Construtor

--- src/livraria/CadastroDeLivros.java
+++ src/livraria/CadastroDeLivros.java
@@ -5,27 +5,25 @@ public class CadastroDeLivros {
 	public static void main(String[] args) {
 		
 	Autor autor = new Autor();
-	autor.nome = "Rodrigo Abreu";
-	autor.email = "rodrigão@gmiau.com";
-	autor.cpf = "040.028.922";
+	autor.setNome("Rodrigo Abreu");
+	autor.setEmail("rodrigo@gmail.com");
+	autor.setCpf("040.028.922");
 	
-	Livro livro = new Livro();
-	livro.nome = "Java 8 Prático";
-	livro.descricao  = "Novos recursos da linguagem";
-	livro.valor = 59.90;
-	livro.isbn = "978-85-66250-46-6";
-	livro.autor = autor;
+	Livro livro = new Livro(autor);
+	livro.setNome("Java 8 Prático");
+	livro.setDescricao("Novos recursos da linguagem");
+	livro.setValor(59.90);
+	livro.setIsbn("978-85-66250-46-6");
 	
 	livro.mostrarDetalhes();
 	
 	System.out.println();
 	
-	Livro outroLivro = new Livro();
-	outroLivro.nome = "Lógica de programação";
-	outroLivro.descricao = "Crie seus primeiros programas";
-	outroLivro.valor = 59.90;
-	outroLivro.isbn = "978-85-66250-22-0";
-	outroLivro.autor = autor;
+	Livro outroLivro = new Livro(autor);
+	outroLivro.setNome("Lógica de programação");
+	outroLivro.setDescricao("Crie seus primeiros programas");
+	outroLivro.setValor(59.90);
+	outroLivro.setIsbn("978-85-66250-22-0");
 	
 	outroLivro.mostrarDetalhes();
 	}
--- src/livraria/Livro.java
+++ src/livraria/Livro.java
@@ -2,11 +2,18 @@ package livraria;
 
 public class Livro {
 	
-	String nome;
-	String descricao;
-	double valor;
-	String isbn;
-	Autor autor;
+	private String nome;
+	private String descricao;
+	private double valor;
+	private String isbn;
+	private Autor autor;
+
+	public Livro (Autor autor){
+		this.autor = autor;
+		this.isbn = "000-00-00000-00-0";
+	}
+	
+	public Livro () {}
 	
 	boolean temAutor(){
 		return this.autor != null;
@@ -27,8 +34,52 @@ public class Livro {
 		System.out.println("---");
 	}
 	
-	public void aplicarDescontoDe(double porcentagem){
+	public boolean aplicarDescontoDe(double porcentagem){
+		if (porcentagem > 0.3){
+			return false;
+		}
 		this.valor -= this.valor * porcentagem;
+		return true;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+	public Autor getAutor() {
+		return autor;
+	}
+
+	public void setAutor(Autor autor) {
+		this.autor = autor;
+	}
+
+	public double getValor() {
+		return valor;
+	}
+
+	public void setValor(double valor) {
+		this.valor = valor;
 	}
 	
 }
