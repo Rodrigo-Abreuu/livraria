228bec57afcedf93240dab39207930ee08146e2c
Rodrigo-Abreuu <77403600+Rodrigo-Abreuu@users.noreply.github.com>
2021-03-03 15:16:26 -0300

    finalizando herança da classe livro

--- src/livraria/RegistroDeVendas.java
+++ src/livraria/RegistroDeVendas.java
@@ -9,13 +9,17 @@ public class RegistroDeVendas {
 		
 		LivroFisico livroFisico = new LivroFisico(autor);
 		livroFisico.setNome("Livro OO");
+		livroFisico.setValor(59.90);
 		
 		Ebook ebook = new Ebook(autor);
 		ebook.setNome("Livro OO");
+		ebook.setValor(29.90);
 		
 		CarrinhoDeCompras carrinho = new CarrinhoDeCompras();
 		carrinho.adiciona(livroFisico);
 		carrinho.adiciona(ebook);
+		
+		System.out.println("Total: " + carrinho.getTotal());
 	}
 
 }
